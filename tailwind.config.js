5a9de2ead886b329366bf4ef052314c0fff1ff1b
Hayk Khachatryan <hi@hayk.io>
2020-06-12 19:59:33 +0400

    tailwind config
    - linting
    - added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   purge: false,
   theme: {
+    colors: {
+      purple: '#7E7F9A',
+      pink: '#fab1a0'
+    },
     container: {
       center: true,
       padding: '1rem'
     }
   },
   variants: {},
-  plugins: [],
+  plugins: []
 }
