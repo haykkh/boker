88c7ad3772637fe96323c695ddf56b9c755cd886
Hayk Khachatryan <hi@hayk.io>
2020-06-12 14:15:23 +0400

    babel fische babel fische (https://www.youtube.com/watch?v=FhcjRybe_t4&ab_channel=FerkoMrkva)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "standard"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-};
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  parser: 'babel-eslint',
+  plugins: ['import', 'react', 'prettier', 'jsx-a11y'],
+  rules: {
+    'no-unused-vars': [2, { varsIgnorePattern: 'React|jsx|h' }],
+    'react/jsx-uses-vars': 2,
+    'no-undef': 0,
+    'import/no-absolute-path': 0
+  }
+}
