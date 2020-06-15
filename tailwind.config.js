b304721610741464f4c18ac6cbc61b729cd9c287
Hayk Khachatryan <hi@hayk.io>
2020-06-15 21:05:50 +0400

    Defined colors as a const so i DRY

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
+const colors = {
+  purple: '#7E7F9A',
+  pink: '#fab1a0',
+  white: '#fff',
+  crimson: 'crimson'
+}
+
 module.exports = {
   purge: false,
   theme: {
-    colors: {
-      purple: '#7E7F9A',
-      pink: '#fab1a0',
-      white: '#fff',
-      crimson: 'crimson'
-    },
+    colors: colors,
     container: {
       center: true,
       padding: '1rem'
     },
     fontFamily: {
       fixedsys: ['fixedsys', 'monospace']
+    },
+    fill: {
+      purple: colors.purple
     }
   },
   variants: {},
