64e2d3eaf2e6ca7cf90a420d9eab5c1b69ff865a
Hayk Khachatryan <hi@hayk.io>
2020-06-16 19:23:01 +0400

    auth.actions.js
    SetAuthAndUser:
      - sets state.authorized and state.user
    
    SetAccessToken:
      - sets state.user.access_token

--- src/js/actions/auth.actions.js
+++ src/js/actions/auth.actions.js
@@ -1,5 +1,13 @@
-export const SetAuth = state => {
+export const SetAuthAndUser = (state, { auth, user }) => {
   const newState = { ...state }
-  newState.authorized = true
+  newState.authorized = auth
+  newState.user = user
+  console.log('user', user)
+  return newState
+}
+
+export const SetAccessToken = (state, token) => {
+  const newState = { ...state }
+  newState.user.access_token = token
   return newState
 }
