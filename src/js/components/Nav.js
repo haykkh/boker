f2aab25e3bce36bfb1619964d49006c0976fee96
Hayk Khachatryan <hi@hayk.io>
2020-06-16 13:02:23 +0400

    Nav.js authorized check
    - added check to only underline current tab if user is authorized

--- src/js/components/Nav.js
+++ src/js/components/Nav.js
@@ -11,7 +11,13 @@ const Nav = ({ state }) => {
       <ul class="flex -mx-3 flex-col sm:flex-row h-32 sm:h-auto justify-around">
         {tabs.map(tab => (
           <li class="flex-1w-1/3 px-3 font-bold text-purple hover:text-pink">
-            <button class={state.currentTab === tab ? 'font-bold underline' : 'font-bold'} onclick={() => SelectTab(state, tab)}>{state.tabs[tab].label}</button>
+            <button class={
+              state.authorized
+                ? state.currentTab === tab
+                  ? 'font-bold underline'
+                  : 'font-bold'
+                : 'font-bold'
+            } onclick={() => SelectTab(state, tab)}>{state.tabs[tab].label}</button>
           </li>
         ))}
       </ul>
