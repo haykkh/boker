a1fb2bd5880ff5cc08ec85ece544d5fdec3876bb
Hayk Khachatryan <hi@hayk.io>
2020-06-16 19:27:12 +0400

    TopUp.js
    displays username for now

--- src/js/components/TopUp.js
+++ src/js/components/TopUp.js
@@ -1,9 +1,9 @@
 import { h } from 'hyperapp'
 
-const TopUp = () => (
+const TopUp = ({ state }) => (
   <section class="flex justify-center items-center" id="topup">
     <content class="container fill-purple w-168 h-168 flex justify-center items-center">
-      <h1 class="text-white text-xl font-bold"> hello loser</h1>
+      <h1 class="text-white text-xl font-bold"> hello {state.user.username} (loser)</h1>
     </content>
   </section>
 )
