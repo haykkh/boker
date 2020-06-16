5833170494fa3336385fe3c61200db5b68c3d93c
Hayk Khachatryan <hi@hayk.io>
2020-06-16 12:42:11 +0400

    Made top up <content> 42rem

--- src/js/components/TopUp.js
+++ src/js/components/TopUp.js
@@ -2,7 +2,7 @@ import { h } from 'hyperapp'
 
 const TopUp = () => (
   <section class="flex justify-center items-center" id="topup">
-    <content class="container fill-purple w-64 h-64 flex justify-center items-center">
+    <content class="container fill-purple w-168 h-168 flex justify-center items-center">
       <h1 class="text-white text-xl font-bold"> hello loser</h1>
     </content>
   </section>
