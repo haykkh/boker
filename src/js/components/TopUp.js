6918ab7707262eb49a1db38f135ab5a514eaded1
Hayk Khachatryan <hi@hayk.io>
2020-06-17 15:23:27 +0400

    TopUp.js
    Added more user params (discriminator, avatar, id)

--- src/js/components/TopUp.js
+++ src/js/components/TopUp.js
@@ -2,8 +2,10 @@ import { h } from 'hyperapp'
 
 const TopUp = ({ state }) => (
   <section class="flex justify-center items-center" id="topup">
-    <content class="container fill-purple w-168 h-168 flex justify-center items-center">
-      <h1 class="text-white text-xl font-bold"> hello {state.user.username} (loser)</h1>
+    <content class="container fill-purple w-168 h-168 flex justify-center items-center flex-col">
+      <h1 class="text-white text-xl font-bold"> hello {state.user.username}#{state.user.discriminator} (loser)</h1>
+      <p>{state.user.id}</p>
+      <img class="rounded-full" src={`https://cdn.discordapp.com/avatars/${state.user.id}/${state.user.avatar}.jpg`}></img>
     </content>
   </section>
 )
