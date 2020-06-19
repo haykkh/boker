b27f6154ce524d92748c07bbcf52f7bd76544d13
Hayk Khachatryan <hi@hayk.io>
2020-06-19 19:50:14 +0400

    Balances.js heyo mr table
    - <Bal />:
      - no more
    
    - round():
      - rounds numbers to 2 dp and always displays 2 dp
    
    - <Title />
      - <th> table header element
    
    - <UserCol />
      - <td> element
      - a specific column from user, eg name/bal etc
    
    - <UserRow />
      - <tr> element
      - Row of user info
    
    - <BalTable />
      - whole table

--- src/js/components/Balances.js
+++ src/js/components/Balances.js
@@ -1,21 +1,57 @@
 import { h } from 'hyperapp'
 import { WholeLoadingFish } from './LoadingFish'
 
-const Bal = ({ state }) => (
-  <section class="flex justify-center items-center mb-8">
-    <content class="text-white flex justify-center items-center flex-col p-16 sm:p-32 blob-tall">
-      {state.tabs.balances.users.map(user => (
-        <p>{user.name}: {user.chip_balance}</p>
-      ))}
-    </content>
-  </section>
+const round = num => Number(Math.round(parseFloat(num + 'e' + 2)) + 'e-' + 2).toFixed(2)
+
+const Title = ({ text }) => (
+  <th class="px-6 py-3 text-xs leading-4 text-left">
+    {text}
+  </th>
+)
+
+const UserCol = ({ text, align }) => (
+  <td class={`px-6 py-4 whitespace-no-wrap text-sm leading-5 ${align}`}>{text}</td>
+)
+
+const UserRow = ({ userData }) => (
+  <tr>
+    <UserCol text={userData.name} />
+    <UserCol text={userData.chip_balance} />
+    <UserCol align="text-right" text={userData.total_balance < 0
+      ? '-€' + round(Math.abs(userData.total_balance))
+      : '€' + round(userData.total_balance) } />
+
+  </tr>
+)
+
+const BalTable = ({ state }) => (
+  <div class="flex text-white justify-center">
+    <div class="overflow-x-auto blob-tall w-auto p-12 sm:p-24 flex justify-center">
+      <div class="overflow-hidden">
+        <table>
+          <thead>
+            <tr>
+              <Title text="name" />
+              <Title text="chips" />
+              <Title text="balance" />
+            </tr>
+          </thead>
+          <tbody>
+            {state.tabs.balances.users.map(user => (
+              <UserRow userData={user} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 )
 
 const Balances = ({ state }) => {
   if (state.gettingDb) {
     return WholeLoadingFish()
   } else if (state.haveDb) {
-    return (<Bal state={state} />)
+    return (<BalTable state={state} />)
   }
 }
 
