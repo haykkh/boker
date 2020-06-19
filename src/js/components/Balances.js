eeede380b93471f2c6d77013b0de835daba93315
Hayk Khachatryan <hi@hayk.io>
2020-06-19 20:12:13 +0400

    Balances.js
    - Added user.total_transactions (top ups) to table
    - some padding changes to fit mobile

--- src/js/components/Balances.js
+++ src/js/components/Balances.js
@@ -4,19 +4,20 @@ import { WholeLoadingFish } from './LoadingFish'
 const round = num => Number(Math.round(parseFloat(num + 'e' + 2)) + 'e-' + 2).toFixed(2)
 
 const Title = ({ text }) => (
-  <th class="px-6 py-3 text-xs leading-4 text-left">
+  <th class="px-3 sm:px-6 py-3 text-xs leading-4 text-left">
     {text}
   </th>
 )
 
 const UserCol = ({ text, align }) => (
-  <td class={`px-6 py-4 whitespace-no-wrap text-sm leading-5 ${align}`}>{text}</td>
+  <td class={`px-3 sm:px-6 py-4 whitespace-no-wrap text-sm leading-5 ${align}`}>{text}</td>
 )
 
 const UserRow = ({ userData }) => (
   <tr>
     <UserCol text={userData.name} />
     <UserCol text={userData.chip_balance} />
+    <UserCol align="text-right" text={'€' + round(Math.abs(userData.total_transactions))} />
     <UserCol align="text-right" text={userData.total_balance < 0
       ? '-€' + round(Math.abs(userData.total_balance))
       : '€' + round(userData.total_balance) } />
@@ -27,12 +28,13 @@ const UserRow = ({ userData }) => (
 const BalTable = ({ state }) => (
   <div class="flex text-white justify-center">
     <div class="overflow-x-auto blob-tall w-auto p-12 sm:p-24 flex justify-center">
-      <div class="overflow-hidden">
+      <div class="overflow-scroll">
         <table>
           <thead>
             <tr>
               <Title text="name" />
               <Title text="chips" />
+              <Title text="top ups" />
               <Title text="balance" />
             </tr>
           </thead>
